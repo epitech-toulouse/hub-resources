ab5139103ac3b9cf0f4b0a7634c67677cee117de
Emily Flion <emneo@kreog.com>
2025-11-18 13:48:20 +0100

    Update avaible targets script
    Signed-off-by: Emily Flion <emneo@kreog.com>

--- available_targets.sh
+++ available_targets.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash jq -p bash
 
-echo -e "Available target for \`nix build\`:\n"
+echo -e "Available targets for \`nix build\`:\n"
 (nix eval .#packages.x86_64-linux --apply builtins.attrNames --json 2> /dev/null) | jq flatten[] -r
